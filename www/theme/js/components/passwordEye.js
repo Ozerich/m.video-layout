3adfb63c79954025b2b7174dd2c08fe61d05a1db
OzerskyVS <ozicoder@gmail.com>
2013-09-26 15:07:20 +0300

    ie fixs

--- www/theme/js/components/passwordEye.js
+++ www/theme/js/components/passwordEye.js
@@ -7,12 +7,14 @@ $.fn.PasswordEye = function (options) {
     var that = this;
     var $item = $(this);
 
-    $item.wrap('<div class="input-eye-wr" style="width: ' + $item.outerWidth() + 'px"/>');
+    var width = $item.outerWidth() - 46;
+
+    $item.wrap('<div class="input-eye-wr" style="width: ' + width + 'px"/>');
     $item.after('<i/>').after('<input style="display: none" type="text"/>');
 
     var $wrapper = $item.parent();
 
-    $wrapper.find('input').width($wrapper.find('input[type=text]').width() - 30);
+    $wrapper.find('input').width(width);
 
     var value = $wrapper.find('input[type=password]').val();
 
@@ -23,7 +25,11 @@ $.fn.PasswordEye = function (options) {
         else {
             $wrapper.find('input[type=text]').val($(this).val());
         }
-    });
+    }).on('focus', function(){
+            $(this).parents('.input-eye-wr').addClass('focus');
+        }).on('blur', function(){
+            $(this).parents('.input-eye-wr').removeClass('focus');
+        });
 
     $wrapper.find('i').on('click', function () {
         $wrapper.toggleClass('input-pass-visible');
