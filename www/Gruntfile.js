b2c345ab3fc8eaf9aa013f01507b58524d7fe464
OzerskyVS <ozicoder@gmail.com>
2013-10-02 16:31:45 +0300

    grunt

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -1,68 +1,69 @@
-module.exports = function(grunt) {
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		// less builder
-		less: {
-			build: {
-				options: {
+        // less builder
+        less: {
+            build: {
+                options: {
                     yuicompress: true
-				},
-				files: {
-					"checkout/dynamic/css/styles.css": "checkout/dynamic/css/styles.less",
-					"thankyou/css/styles.css": "thankyou/css/styles.less",
-					"auth/css/styles.css": "auth/css/styles.less"
-				}
-			}
-		},
+                },
+                files: {
+                    "checkout/dynamic/css/styles.css": "checkout/dynamic/css/styles.less",
+                    "thankyou/css/styles.css": "thankyou/css/styles.less",
+                    "auth/css/styles.css": "auth/css/styles.less"
+                }
+            }
+        },
 
         uglify: {
             plugins: {
-                options: {
-                    preserveComments: 'all'
-                },
                 files: {
-                    "theme/js/thirdparty/min/jquery-1.8.1.min.js":"theme/js/thirdparty/jquery-1.8.1.min.js",
-                    "theme/js/thirdparty/min/html5shiv.min.js":"theme/js/thirdparty/html5shiv.js",
-                    "theme/js/thirdparty/min/jquery.blockUI.min.js":"theme/js/thirdparty/jquery.blockUI.js",
-                    "theme/js/thirdparty/min/jquery.jscrollpane.min.js":"theme/js/thirdparty/jquery.jscrollpane.min.js",
-                    "theme/js/thirdparty/min/jquery.maskedinput.min.js":"theme/js/thirdparty/jquery.maskedinput.js",
-                    "theme/js/thirdparty/min/jquery.mousewheel.min.js":"theme/js/thirdparty/jquery.mousewheel.js",
-                    "theme/js/thirdparty/min/jquery.placeholder.min.js":"theme/js/thirdparty/jquery.placeholder.js"
+                    "theme/js/thirdparty/min/jquery-1.8.1.min.js": "theme/js/thirdparty/jquery-1.8.1.min.js",
+                    "theme/js/thirdparty/min/html5shiv.min.js": "theme/js/thirdparty/html5shiv.js",
+                    "theme/js/thirdparty/min/jquery.blockUI.min.js": "theme/js/thirdparty/jquery.blockUI.js",
+                    "theme/js/thirdparty/min/jquery.jscrollpane.min.js": "theme/js/thirdparty/jquery.jscrollpane.min.js",
+                    "theme/js/thirdparty/min/jquery.maskedinput.min.js": "theme/js/thirdparty/jquery.maskedinput.js",
+                    "theme/js/thirdparty/min/jquery.mousewheel.min.js": "theme/js/thirdparty/jquery.mousewheel.js",
+                    "theme/js/thirdparty/min/jquery.placeholder.min.js": "theme/js/thirdparty/jquery.placeholder.js"
                 }
             },
             components: {
                 files: {
-                    "theme/js/components/min/jquery-1.8.1.min.js":"theme/js/components/autocomplete.js",
-                    "theme/js/components/min/calendar.min.js":"theme/js/components/calendar.js",
-                    "theme/js/components/min/checkbox.min.js":"theme/js/components/checkbox.js",
-                    "theme/js/components/min/passwordEye.min.js":"theme/js/components/passwordEye.js",
-                    "theme/js/components/min/switch.min.js":"theme/js/components/switch.js"
+                    "theme/js/components/min/autocomplete.js": "theme/js/components/autocomplete.js",
+                    "theme/js/components/min/calendar.min.js": "theme/js/components/calendar.js",
+                    "theme/js/components/min/checkbox.min.js": "theme/js/components/checkbox.js",
+                    "theme/js/components/min/passwordEye.min.js": "theme/js/components/passwordEye.js",
+                    "theme/js/components/min/switch.min.js": "theme/js/components/switch.js"
                 }
             }
         },
 
         concat: {
-            options:{
+            options: {
                 separator: ";\n"
             },
-            plugins:{
+            plugins: {
                 src: ["theme/js/thirdparty/min/*.js"],
                 dest: "theme/js/plugins.js"
             },
-            components:{
-                src: ["theme/js/components/min/*.js"],
+            components: {
+                src: ["theme/js/components/*.js"],
                 dest: "theme/js/components.js"
+            },
+            components_min: {
+                src: ["theme/js/components/min/*.js"],
+                dest: "theme/js/components.min.js"
             }
         }
 
-	});
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-	// Default task(s).
-	grunt.registerTask('build',['less:build', 'uglify:plugins', 'uglify:components', 'concat:plugins', 'concat:components']);
+    // Default task(s).
+    grunt.registerTask('build', ['less:build', 'uglify:plugins', 'uglify:components', 'concat:plugins', 'concat:components_min', 'concat:components']);
 };
--- www/theme/js/components.js
+++ www/theme/js/components.js
@@ -1,5 +1,400 @@
-function Calendar(a){var b=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],c=$(a);if(0===c.length)return null;var d="";this.setData=function(a,e,f){d='<div class="calendar">',d+='<p class="month-name">'+b[a.getMonth()]+"</p>",d+="<table><thead><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr></thead><tbody>";for(var g=new Date(a.getFullYear(),a.getMonth(),a.getDate()-(0==a.getDay()?7:a.getDay())+1),h=new Date(e.getFullYear(),e.getMonth(),e.getDate()+(7-(0==e.getDay()?7:e.getDay()))),i=new Date,j=new Date(g);;){1==j.getDay()&&(d+="<tr>");var k=[];1==j.getDay()&&k.push("first"),(0==j.getDay()||6==j.getDay())&&k.push("weekend"),(a>j||j>e)&&k.push("disabled"),j.getDate()==i.getDate()&&j.getMonth()==i.getMonth()&&j.getFullYear()==i.getFullYear()&&k.push("today");var l=(j.getDate()<10?"0"+j.getDate():j.getDate())+"."+(j.getMonth()<10?"0"+j.getMonth():j.getMonth())+"."+j.getFullYear(),m=l in f?f[l].toString():"бесплатно";if(d+='<td class="'+k.join(" ")+'"><span class="day">'+(j.getDate()<10?"0"+j.getDate():j.getDate())+'</span><span class="price">'+m+"</span></td>",0==j.getDay()&&(d+="</tr>"),j=new Date(j.getFullYear(),j.getMonth(),j.getDate()+1),j.getFullYear()>h.getFullYear())break;if(j.getFullYear()==h.getFullYear()&&j.getMonth()>h.getMonth())break;if(j.getFullYear()==h.getFullYear()&&j.getMonth()==h.getMonth()&&j.getDate()>h.getDate())break}d+="</tbody></table>",h.getMonth()!=g.getMonth()&&(d+='<p class="month-name month-name-bottom">'+b[h.getMonth()]+"</p>"),d+="</div>",c.html(d)}}$.fn.Calendar=function(a){function b(a,b){return Math.abs((b-a)/864e5)}function c(a){var b=a.getFullYear(),c=a.getMonth(),d=a.getDate();return b+(10>c?"0":"")+c+(10>d?"0":"")+d}var d=this,e=$(this),f=new Date;f=new Date(f.getFullYear(),f.getMonth(),f.getDate(),0,0,0),a=$.extend({onChange:function(){}},a),this.checkDate=function(a){var c=!0;if(a.getFullYear()<f.getFullYear()||a.getMonth()<f.getMonth()||a.getDate()<f.getDate())c=!1;else{var d=b(a,f);d>14&&(c=!1)}return c},this.renderHTML=function(b,g){var h,i,j=0,k=function(a,b){var e=g,h=b;0>h&&(h=11,e--),h>11&&(h=0,e++);var i=new Date(e,h,a),k="";if(0==j&&(k='<tr class="days">'),d.checkDate(i)){var l=[];(6==i.getDay()||0==i.getDay())&&l.push("weekend"),i.getDate()==f.getDate()&&i.getMonth()==f.getMonth()&&i.getFullYear()==f.getFullYear()&&l.push("today"),k+='<td data-date="'+c(i)+'" class="calendar-day '+l.join(" ")+'"><span>'+a+"</span></td>"}else k+='<td data-date="'+c(i)+'" class="calendar-day prev-month">'+a+"</td>";return j++,0==j%7&&(j=0,k+="</tr>"),k},l='<table width="100%" cellpadding="0" cellspacing="0" class="calendar-table"><tr class="weekdays"><td class="calendar-day first">Пн</td><td class="calendar-day">Вт</td><td class="calendar-day">Ср</td><td class="calendar-day">Чт</td><td class="calendar-day">Пт</td><td class="calendar-day weekend">Сб</td><td class="calendar-day weekend last">Вс</td></tr>';l+='<tr class="days first-line">';var m=new Date(g,b,1).getDay();m=0==m?7:m;var n=m-1,o=new Date(g,b,0).getDate();for(h=n;h>0;h--)l+=k(o-h+1,b-1);for(i=1,h=m;7>=h;h++,i++)l+=k(i,b);var p=new Date(g,b+1,0),q=0==p.getDay()?7:p.getDay(),r=p.getDate()-q,s=7-m+2;for(i=1,h=s;r>=h;h++,i++)l+=k(h,b);for(h=r+1;h<=new Date(g,b+1,0).getDate();h++)l+=k(h,b);for(h=1;7-q>=h;h++)l+=k(h,b+1);l+="</table>",e.html(l),e.find(".days .calendar-day").not(".prev-month").click(function(){var b=$(this).data("date").toString();b=new Date(b.substr(0,4),b.substr(4,2),b.substr(6,2)),a.onChange(b)})},this.renderHTML(f.getMonth(),f.getFullYear()),$(document).click(function(a){0===$(a.target).parents(".calendar-table").length&&e.parent().hide()})};;
-$.fn.Checkbox=function(a){if(a=a||function(){},!$(this).hasClass("checkbox"))throw"Class 'checkbox' is not set'";return $(this).on("click",function(){return $(this).toggleClass("checkbox-checked"),a($(this).hasClass("checkbox-checked")),$(this).trigger("change"),!1}),this};;
-$.fn.AutoComplete=function(a){var b=$(this);a=$.extend({url:"",prepareItem:function(){return""},onSelect:function(){},noItemsHtml:""},a);var c=$('<div class="autocomplete-list" style="display: none"></div>');$("body").append(c);var d=null,e=function(){var d=b.val();d.length<2||(c.css({top:b.offset().top+b.outerHeight()+2+"px",left:b.offset().left+"px",width:b.outerWidth()+"px",position:"absolute"}),$.get(a.url+d,function(e){var f=$("<ul></ul>"),g=0;for(var h in e){if(++g>3)break;var i=$('<li data-object="'+h+'">'+a.prepareItem(e[h],d)+"</li>");f.append(i)}var j=f.find("li").length?"<ul>"+f.html()+"</ul>":a.noItemsHtml;c.show().html(j),c.find("li").click(function(){return b.val(a.onSelect(e[$(this).data("object")])),c.hide(),!1})}))};b.keyup(function(){window.clearTimeout(d),$(this).val().length<2&&c.hide(),d=window.setTimeout(function(){e()},100)}),$(document).click(function(a){0===$(a.target).parents(".autocomplete-list").length&&c.hide()})};;
-$.fn.PasswordEye=function(){$.browser.msie&&$.browser.version<=9;var a=$(this),b=a.outerWidth()-46;a.wrap('<div class="input-eye-wr" style="width: '+b+'px"/>'),a.after("<i/>").after('<input style="display: none" type="text"/>');var c=a.parent();c.find("input").width(b),c.find("input[type=password]").val(),c.find("input").on("change",function(){"text"==$(this).attr("type")?c.find("input[type=password]").val($(this).val()):c.find("input[type=text]").val($(this).val())}).on("focus",function(){$(this).parents(".input-eye-wr").addClass("focus")}).on("blur",function(){$(this).parents(".input-eye-wr").removeClass("focus")}),c.find("i").on("click",function(){c.toggleClass("input-pass-visible"),c.find("input").toggle()})};;
-$.fn.Switch=function(a){if(a=a||function(){},!$(this).hasClass("switch-container"))throw"Class 'switch-container' is not set'";var b=$(this).find(".switch-item");return 0===b.length?this:(b.each(function(){$(this).on("click",function(){return $(this).hasClass("selected")?!1:(b.filter(".selected").removeClass("selected"),$(this).addClass("selected"),a($(this)),void 0)})}),this)};
+$.fn.AutoComplete = function (options) {
+    var that = this;
+    var $item = $(this);
+
+    options = $.extend({
+        url: '',
+        prepareItem: function (object) {
+            return '';
+        },
+        onSelect: function(object){
+
+        },
+        noItemsHtml: ''
+    }, options);
+
+    var $list = $('<div class="autocomplete-list" style="display: none"></div>');
+
+    $('body').append($list);
+
+    var timeout = null;
+
+    var LoadList = function () {
+        var query = $item.val();
+        if (query.length < 2) {
+            return;
+        }
+
+        $list.css({
+            top: $item.offset().top + $item.outerHeight() + 2 + 'px',
+            left: $item.offset().left + 'px',
+            width: $item.outerWidth() + 'px',
+            position: 'absolute'
+        });
+
+        $.get(options.url + query, function (data) {
+            var list = $('<ul></ul>');
+            var count = 0;
+            for(var i in data){
+                if(++count > 3)break;
+
+                var item = $('<li data-object="'+i+'">' + options.prepareItem(data[i], query) + '</li>');
+
+                list.append(item);
+            }
+
+            var html = list.find('li').length ?  '<ul>' + list.html() + '</ul>' : options.noItemsHtml;
+
+            $list.show().html(html);
+
+            $list.find('li').click(function(){
+
+                $item.val(options.onSelect(data[$(this).data('object')]));
+                $list.hide();
+
+                return false;
+            });
+        });
+    };
+
+    $item.keyup(function () {
+        window.clearTimeout(timeout);
+        if ($(this).val().length < 2) {
+            $list.hide();
+        }
+        timeout = window.setTimeout(function () {
+            LoadList();
+        }, 100);
+    });
+
+    $(document).click(function(event){
+        if($(event.target).parents('.autocomplete-list').length === 0){
+            $list.hide();
+        }
+    })
+
+};;
+function Calendar(_selector) {
+    var that = this;
+
+    var month_labels = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+    var $elem = $(_selector);
+    if ($elem.length === 0) {
+        return null;
+    }
+
+    var html = '';
+
+    this.setData = function (_date_from, _date_to, _prices) {
+        html = '<div class="calendar">';
+
+        html += '<p class="month-name">' + month_labels[_date_from.getMonth()] + '</p>';
+        html += '<table><thead><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr></thead><tbody>';
+
+        var start = new Date(_date_from.getFullYear(), _date_from.getMonth(), _date_from.getDate() - (_date_from.getDay() == 0 ? 7 : _date_from.getDay()) + 1);
+        var end = new Date(_date_to.getFullYear(), _date_to.getMonth(), _date_to.getDate() + (7 - (_date_to.getDay() == 0 ? 7 : _date_to.getDay())));
+        var today = new Date();
+        var date = new Date(start);
+
+        while (true) {
+
+            if (date.getDay() == 1) {
+                html += '<tr>';
+            }
+
+            var classes = [];
+            if (date.getDay() == 1) {
+                classes.push('first');
+            }
+
+            if (date.getDay() == 0 || date.getDay() == 6) {
+                classes.push('weekend');
+            }
+
+            if (date < _date_from || date > _date_to) {
+                classes.push('disabled');
+            }
+
+            if (date.getDate() == today.getDate() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()) {
+                classes.push('today');
+            }
+
+
+            var date_str = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + '.' + (date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()) + '.' + date.getFullYear();
+            var price = date_str in _prices ? _prices[date_str].toString() : 'бесплатно';
+            html += '<td class="' + classes.join(' ') + '"><span class="day">' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + '</span><span class="price">' + price + '</span></td>';
+
+            if (date.getDay() == 0) {
+                html += '</tr>';
+            }
+
+            date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+
+            if (date.getFullYear() > end.getFullYear())break;
+            if (date.getFullYear() == end.getFullYear() && date.getMonth() > end.getMonth())break;
+            if (date.getFullYear() == end.getFullYear() && date.getMonth() == end.getMonth() && date.getDate() > end.getDate())break;
+        }
+
+        html += '</tbody></table>';
+
+        if (end.getMonth() != start.getMonth()) {
+            html += '<p class="month-name month-name-bottom">' + month_labels[end.getMonth()] + '</p>';
+        }
+
+        html += '</div>'
+        $elem.html(html);
+    };
+
+
+}
+
+$.fn.Calendar = function (options) {
+    var that = this;
+    var $calendar = $(this);
+
+    var today = new Date();
+    today = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+
+    options = $.extend({
+        onChange: function () {
+
+        }
+    }, options);
+
+    function daydiff(first, second) {
+        return Math.abs((second - first) / (1000 * 60 * 60 * 24));
+    }
+
+    this.checkDate = function (date) {
+        var isEnabled = true;
+
+        if (date.getFullYear() < today.getFullYear() || date.getMonth() < today.getMonth() || date.getDate() < today.getDate()) {
+            isEnabled = false;
+        }
+        else {
+            var diff_days = daydiff(date, today);
+            if (diff_days > 14) {
+                isEnabled = false;
+            }
+        }
+
+        return isEnabled
+    };
+
+    function dateToData(date) {
+
+        var year = date.getFullYear();
+        var month = date.getMonth();
+        var day = date.getDate();
+
+        return year + (month < 10 ? '0' : '') + month + (day < 10 ? '0' : '') + day;
+    }
+
+    this.renderHTML = function (month, year) {
+
+
+        var printCount = 0;
+
+        var print_day = function (_day, _month) {
+            var currentYear = year;
+            var currentMonth = month;
+
+            var printYear = year;
+            var printMonth = _month;
+
+            if (printMonth < 0) {
+                printMonth = 11;
+                printYear--;
+            }
+
+            if (printMonth > 11) {
+                printMonth = 0;
+                printYear++;
+            }
+
+            var printDate = new Date(printYear, printMonth, _day);
+
+
+            var result = '';
+            if (printCount == 0) {
+                result = '<tr class="days">';
+            }
+
+            if (that.checkDate(printDate)) {
+                var classes = [];
+
+                if (printDate.getDay() == 6 || printDate.getDay() == 0) {
+                    classes.push('weekend');
+                }
+
+                if (printDate.getDate() == today.getDate() && printDate.getMonth() == today.getMonth() && printDate.getFullYear() == today.getFullYear()) {
+                    classes.push('today');
+                }
+
+                result += '<td data-date="' + dateToData(printDate) + '" class="calendar-day ' + classes.join(' ') + '"><span>' + _day + '</span></td>';
+            }
+            else {
+                result += '<td data-date="' + dateToData(printDate) + '" class="calendar-day prev-month">' + _day + '</td>';
+            }
+            printCount++;
+            if (printCount % 7 == 0) {
+                printCount = 0;
+                result += '</tr>';
+            }
+
+            return result;
+        };
+
+        var i, j;
+
+        var html = '<table width="100%" cellpadding="0" cellspacing="0" class="calendar-table"><tr class="weekdays">' +
+            '<td class="calendar-day first">Пн</td><td class="calendar-day">Вт</td><td class="calendar-day">Ср</td>' +
+            '<td class="calendar-day">Чт</td><td class="calendar-day">Пт</td>' +
+            '<td class="calendar-day weekend">Сб</td><td class="calendar-day weekend last">Вс</td>' +
+            '</tr>';
+
+        html += '<tr class="days first-line">';
+
+        var firstDay = new Date(year, month, 1).getDay();
+        firstDay = firstDay == 0 ? 7 : firstDay;
+        var needFromLastMonth = firstDay - 1;
+
+        var lastDayPrevMonth = new Date(year, month, 0).getDate();
+
+        for (i = needFromLastMonth; i > 0; i--) {
+            html += print_day(lastDayPrevMonth - i + 1, month - 1);
+        }
+
+        for (j = 1, i = firstDay; i <= 7; i++, j++) {
+            html += print_day(j, month);
+        }
+
+        var lastDayDate = new Date(year, month + 1, 0);
+        var lastDay = lastDayDate.getDay() == 0 ? 7 : lastDayDate.getDay();
+
+        var lastCycleDay = lastDayDate.getDate() - lastDay;
+        var startCycleDay = 7 - firstDay + 2;
+
+        for (j = 1, i = startCycleDay; i <= lastCycleDay; i++, j++) {
+            html += print_day(i, month);
+        }
+
+        for (i = lastCycleDay + 1; i <= new Date(year, month + 1, 0).getDate(); i++) {
+            html += print_day(i, month);
+        }
+
+        for (i = 1; i <= 7 - lastDay; i++) {
+            html += print_day(i, month + 1);
+        }
+
+        html += '</table>';
+
+        $calendar.html(html);
+
+        $calendar.find('.days .calendar-day').not('.prev-month').click(function () {
+            var date = $(this).data('date').toString();
+            date = new Date(date.substr(0, 4), date.substr(4, 2), date.substr(6, 2));
+            options.onChange(date);
+        });
+
+    };
+
+    this.renderHTML(today.getMonth(), today.getFullYear());
+
+    $(document).click(function (event) {
+        if ($(event.target).parents('.calendar-table').length === 0) {
+            $calendar.parent().hide();
+        }
+    })
+};
+;
+$.fn.Checkbox = function (onChange) {
+
+    onChange = onChange || function () {
+    };
+
+    if (!($(this).hasClass('checkbox'))) {
+        throw "Class 'checkbox' is not set'";
+    }
+
+    $(this).on('click', function () {
+
+        $(this).toggleClass('checkbox-checked');
+        onChange($(this).hasClass('checkbox-checked'));
+
+        $(this).trigger('change');
+
+        return false;
+    });
+
+    return this;
+};;
+$.fn.PasswordEye = function (options) {
+
+    if ($.browser.msie && $.browser.version <= 9) {
+        // return;
+    }
+
+    var that = this;
+    var $item = $(this);
+
+    var width = $item.outerWidth() - 46;
+
+    $item.wrap('<div class="input-eye-wr" style="width: ' + width + 'px"/>');
+    $item.after('<i/>').after('<input style="display: none" type="text"/>');
+
+    var $wrapper = $item.parent();
+
+    $wrapper.find('input').width(width);
+
+    var value = $wrapper.find('input[type=password]').val();
+
+    $wrapper.find('input').on('change',function () {
+        if ($(this).attr('type') == 'text') {
+            $wrapper.find('input[type=password]').val($(this).val());
+        }
+        else {
+            $wrapper.find('input[type=text]').val($(this).val());
+        }
+    }).on('focus',function () {
+            $(this).parents('.input-eye-wr').addClass('focus');
+        }).on('blur', function () {
+            $(this).parents('.input-eye-wr').removeClass('focus');
+        });
+
+    $wrapper.find('i').on('click', function () {
+        $wrapper.toggleClass('input-pass-visible');
+        $wrapper.find('input').toggle();
+    });
+
+};;
+$.fn.Switch = function (onChange) {
+
+    onChange = onChange || function () {
+    };
+
+    if (!($(this).hasClass('switch-container'))) {
+        throw "Class 'switch-container' is not set'";
+    }
+
+    var children = $(this).find('.switch-item');
+    if (children.length === 0) {
+        return this;
+    }
+
+    children.each(function () {
+        $(this).on('click', function () {
+            if ($(this).hasClass('selected')) {
+                return false;
+            }
+
+            children.filter('.selected').removeClass('selected');
+            $(this).addClass('selected');
+
+            onChange($(this));
+        });
+    });
+
+    return this;
+};
